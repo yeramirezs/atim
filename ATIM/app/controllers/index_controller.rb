f48301657b6a8ee88508e86a71b48cefb4d3c056
Drainfort <ja.romero940@uniandes.edu.co>
2013-02-18 11:51:26 -0500

    a

--- ATIM/app/controllers/index_controller.rb
+++ ATIM/app/controllers/index_controller.rb
@@ -3,12 +3,14 @@ class IndexController < ApplicationController
   	
   	if( !params[:email])
   		flash[:notice] = "No ingreso un usuario valido"
+  		redirect_to root_path
   	else
   		@teacher = Teacher.search(params[:email])
   		if(!@teacher)
   			flash[:notice] = "No ingreso un usuario valido"
+  			redirect_to root_path
   		else
-  			flash[:notice] = ""+@teacher.email
+  			flash[:notice] = ""+@teacher.email+" "+@teacher.theses.to_s
   		end
   	end
 
