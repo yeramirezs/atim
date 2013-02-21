528f78f150e6066557337259add492b22043556e
Drainfort <ja.romero940@uniandes.edu.co>
2013-02-21 12:36:11 -0500

    validaciones

--- ATIM/app/models/thesis.rb
+++ ATIM/app/models/thesis.rb
@@ -1,9 +1,9 @@
 class Thesis < ActiveRecord::Base
 	validates_presence_of :title, :description, :teacher_id
 	attr_accessible :title, :description, :teacher_id
-
 	belongs_to :teacher
-
+	validates_length_of :title, :minimum => 3
+	validates_length_of :description, :minimum => 3
 	validates :title, :length => { :maximum => 140 }
 
 
