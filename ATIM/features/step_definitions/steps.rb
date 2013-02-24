80c67ca11f0fa68b674f1309f799cd1640a8c6db
drainfort <ja.romero940@uniandes.edu.co>
2013-02-24 16:50:37 -0500

    pruebas completas

--- ATIM/features/step_definitions/steps.rb
+++ ATIM/features/step_definitions/steps.rb
@@ -1,7 +1,10 @@
-Given /^I am on (.+)$/ do |page_name|
+Given /^I am on "([^\"]*)"$/ do |page_name|
   visit "/"+page_name
 end
 
+Given /^I visit a tesis with "([^\"]*)","([^\"]*)"$/ do |prof, tesis|
+   visit "/theses/"+prof+"?id2="+tesis
+end
 
 When /^I enter with the username "([^\"]*)"$/ do |username|
   visit root_path
@@ -19,7 +22,9 @@ Then /^I fill the thesis with "([^\"]*)","([^\"]*)"$/ do |title, description|
   click_button("Create Thesis")
 end
 
-
+Then /^press "([^\"]*)"$/ do |text|
+  click_button(text)
+end
 
 Then /^I should see "([^\"]*)"$/ do |text|
   page.should have_content(text)
@@ -29,4 +34,7 @@ Then /^I should be on (.+)$/ do |page_name|
   current_path.should == "/"+page_name
 end
 
+Then /^I cant see the button "([^\"]*)"$/ do |name|
+  page.should_not have_button(name)
+end
 
