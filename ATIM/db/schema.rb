231194fa22146d994b368bcc2cca7bb4c1703fc3
drainfort <ja.romero940@uniandes.edu.co>
2013-03-12 16:21:25 -0500

    nuevo estilo

--- ATIM/db/schema.rb
+++ ATIM/db/schema.rb
@@ -13,13 +13,6 @@
 
 ActiveRecord::Schema.define(:version => 20130311050645) do
 
-  create_table "books", :force => true do |t|
-    t.string   "author"
-    t.string   "name"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
-  end
-
   create_table "commitments", :force => true do |t|
     t.string   "title",                          :null => false
     t.text     "description"
